f3c21349f8ab929b323524a93c9b23331c84d3ca
thiliniyatanwala <tmyatanwala@gmail.com>
2021-01-28 14:02:37 +1100

    2 nd commit

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,44 @@
 public class Employee {
+    int eid;
+    String name;
+    Address address;
+
+    public Employee(){}
+
+    public Employee(Address address) {
+        this.address = address;
+    }
+
+    public int getEid() {
+        return eid;
+    }
+
+    public void setEid(int eid) {
+        this.eid = eid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "eid=" + eid +
+                ", name='" + name + '\'' +
+                ", address=" + address +
+                '}';
+    }
 }
--- src/Employer.java
+++ src/Employer.java
@@ -1,5 +1,14 @@
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
 public class Employer {
     public static void main(String[] args) {
+        Resource resource = new ClassPathResource("dataXML.xml");
+        BeanFactory factory = new XmlBeanFactory(resource);
+        Employee e2 = (Employee) factory.getBean("emp1");
+        System.out.println("Employee 2 Details" + e2);
 
     }
 }
