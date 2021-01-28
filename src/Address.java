5ef6bd723c0e5a33f1e9f8c307a728438523c1f6
thiliniyatanwala <tmyatanwala@gmail.com>
2021-01-28 14:06:05 +1100

    3 rd Commit

---
+++ src/Address.java
@@ -0,0 +1,48 @@
+public class Address {
+    //Attributes
+    String city;
+    String state;
+    int zipcode;
+
+    //Methods
+    public Address(){}
+
+    public Address(String city, String state, int zipcode) {
+        this.city = city;
+        this.state = state;
+        this.zipcode = zipcode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(int zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zipcode=" + zipcode +
+                '}';
+    }
+}
